52bc9233fd932780d8a056a6dc5a6e29d270f402
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-03-18 11:51:37 -0500

    added js

--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,85 @@
 
-$(document).ready(function()  {
-  var questions = [ 
-      {question: "What is 1 + 1?",
-      answers:[
+$(document).ready(function () {
+  var questions = [
+    {
+      question: "What is 1 + 1?",
+      answers: [
         "2",
         "3",
         "4",
         "5"
-      ], 
+      ],
       id: "question-1",
       correctAnswer: 0
-},
-{question: "What is 2 + 2?",
-      answers:[
+    },
+    {
+      question: "What is 2 + 2?",
+      answers: [
         "2",
         "3",
         "4",
         "5"
-      ], 
+      ],
       id: "question-2",
       correctAnswer: 2
-},
-{question: "What is 3 + 3?",
-      answers:[
+    },
+    {
+      question: "What is 3 + 3?",
+      answers: [
         "3",
         "4",
         "5",
         "6"
-      ], 
+      ],
       id: "question-3",
       correctAnswer: 3
     }
-]  
-var timeRemaining = 120;
-function countdown() {
+  ]
+  var timeRemaining = 120;
+  function countdown() {
     timeRemaining--;
     $("#timeRemain").text(timeRemaining + "Seconds Left");
-    if (timeRemaining === 0) 
-    {
-        stop()
-        alert("Time is up!") 
-        checkAnswers()
+    if (timeRemaining === 0) {
+      stop()
+      alert("Time is up!")
+      checkAnswers()
     }
-}
-function run() {
-    counter = setInterval(countdown,1000)
-}
-function stop() {
+  }
+  function run() {
+    counter = setInterval(countdown, 1000)
+  }
+  function stop() {
     clearInterval(counter)
+  }
+  run()
+
+  function questionForm(data) {
+    var questionString = "<form>" + data.question + "<br>"
+    var possibleAnswers = data.answers;
+    for (var i = 0; i < possibleAnswers.length; i++) {
+      var possibleAnswer = possibleAnswers[i];
+      questionString = questionString + "<input type = 'radio' name = " + data.id + " value = " + i + ">" + possibleAnswer
+    }
+    return questionString + "</form><br>" 
+  }
+  window.formTemplate = questionForm
+
+  function buildQuestions() {
+    var questions2 = questions
+    for (var i = 0; i < questions2.length; i++)  {
+      questions2.splice(Math.floor(Math.random()* questions2.length),1)
+    }
+    var questionHTML = ""
+    for (var i = 0; i < questions2.length; i++)  {
+      questionHTML = questionHTML + questionForm(questions2[i])
+    }
+    $("#questions").append(questionHTML)
+  }
+buildQuestions()
+
+function checkAnswers()  {
+  
 }
-    run()
-    
 
 
 
@@ -60,7 +89,4 @@ function stop() {
 
 
 
-
-    
-    
- })
+})
